1841c8f64f957f041e43f4d630b8e6878a451f47
singhprakhar802 <90647871+singhprakhar802@users.noreply.github.com>
2023-09-21 19:52:53 +0530

    Time: 15 ms (15.52%), Space: 14.3 MB (7.9%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -7,28 +7,18 @@ public:
 
         for(int i=0;i<arr.size();i++)
         {
-            // if(ump.find(arr[i])==ump.end())
-            // {
-            //     ump[arr[i]].push_back(i);
-            // }
-            // else
-            // {
-                if(ump[arr[i]].size()==arr[i]-1)
-                {
-                    ump[arr[i]].push_back(i);
-                    ans.push_back(ump[arr[i]]);
-                    ump.erase(arr[i]);
-                }
-                else
-                {
-                    ump[arr[i]].push_back(i);
-                }
-        }     
-
-        // for(auto it = ump.begin();it!=ump.end();it++)
-        // {
-        //     ans.push_back((*it).second);
-        // }
+            if(ump[arr[i]].size()==arr[i]-1)
+            {
+                ump[arr[i]].push_back(i);
+                ans.push_back(ump[arr[i]]);
+                ump.erase(arr[i]);
+            }
+            else
+            {
+                ump[arr[i]].push_back(i);
+            }
+        }
+        
         return ans;
     }
 };
