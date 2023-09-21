e9641a61e49eb504469083184717b0bfb0bc3657
singhprakhar802 <90647871+singhprakhar802@users.noreply.github.com>
2023-09-21 19:51:45 +0530

    Time: 14 ms (21.78%), Space: 14.5 MB (5.78%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -2,35 +2,33 @@ class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& arr) 
     {
-
         vector<vector<int>> ans;
         unordered_map<int,vector<int>> ump;
 
         for(int i=0;i<arr.size();i++)
         {
-            if(ump.find(arr[i])==ump.end())
-            {
-                ump[arr[i]].push_back(i);
-            }
-            else
-            {
-                if(ump[arr[i]].size()<arr[i])
+            // if(ump.find(arr[i])==ump.end())
+            // {
+            //     ump[arr[i]].push_back(i);
+            // }
+            // else
+            // {
+                if(ump[arr[i]].size()==arr[i]-1)
                 {
                     ump[arr[i]].push_back(i);
+                    ans.push_back(ump[arr[i]]);
+                    ump.erase(arr[i]);
                 }
                 else
                 {
-                    ans.push_back(ump[arr[i]]);
-                    ump.erase(arr[i]);
                     ump[arr[i]].push_back(i);
                 }
-            }
         }     
 
-        for(auto it = ump.begin();it!=ump.end();it++)
-        {
-            ans.push_back((*it).second);
-        }
+        // for(auto it = ump.begin();it!=ump.end();it++)
+        // {
+        //     ans.push_back((*it).second);
+        // }
         return ans;
     }
 };
