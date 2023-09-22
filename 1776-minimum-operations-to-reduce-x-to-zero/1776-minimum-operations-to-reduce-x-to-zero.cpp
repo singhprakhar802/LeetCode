54953415e740a925443a6504efc03461a1f7ab00
singhprakhar802 <90647871+singhprakhar802@users.noreply.github.com>
2023-09-22 22:13:49 +0530

    Time: 188 ms (26.18%), Space: 109.7 MB (21.54%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,41 +1,64 @@
 class Solution {
 public:
+
+
+    int find(vector<int>& psum ,int t)
+    {
+        int s = 0;
+        int e = psum.size()-1;
+        while(s <= e)
+        {
+            int mid = (s+e)/2;
+
+            if(psum[mid]==t)
+                return mid;
+            else if(psum[mid] > t)
+            {
+                e = mid -1;
+            }
+            else
+            {
+                s = mid+1;
+            }
+        }
+        return -1;
+    }
     int minOperations(vector<int>& nums, int x) 
     {
         int ans = INT_MAX;
-        unordered_map<int,int> ump;
 
+        vector<int> psum ;
         int sum = 0;
-        ump[0]=sum;
-
+        psum.push_back(0);
         for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
-            ump[sum]=i+1;
+            psum.push_back(sum);
         }
+        int op1 = find(psum,x);
+       cout<<op1<<endl;
+        if(op1!=-1)
+            ans = min(ans,(op1));
+
         sum = 0;
-        if(ump.find(x)!=ump.end())
-        {
-            ans = min(ans,ump[x]);
-        }
-        for(int i=nums.size()-1;i>=0;i--)
+        for(int i = nums.size()-1;i>=0;i--)
         {
             sum+=nums[i];
             if(sum>x)
                 break;
-            
+
             int t = x - sum;
-            if(ump.find(t)!=ump.end())
+
+            int index = find(psum,t);
+            // cout<<index<<" "<<sum<<endl;
+            if(index!=-1 && index-1 < i)
             {
-                int index = ump[t];
-                if(index-1 < i)
-                {
-                    int op = (nums.size()-i)+index;
-                    ans= min(ans,op);
-                }
+                int op = (nums.size()-i)+index;
+                ans=min(ans,op);
             }
         }
 
+        // cout<<psum[2]<<endl;
         if(ans==INT_MAX)
             return -1;
         return ans;
