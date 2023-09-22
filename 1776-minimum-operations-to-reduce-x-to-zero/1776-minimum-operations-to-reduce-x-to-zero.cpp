0154d9c9c160f527c96d318170bca3f051711b3a
singhprakhar802 <90647871+singhprakhar802@users.noreply.github.com>
2023-09-22 21:54:57 +0530

    Time: 344 ms (14.07%), Space: 168.3 MB (11.72%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -4,65 +4,40 @@ public:
     {
         int ans = INT_MAX;
         unordered_map<int,int> ump;
-        unordered_map<int,int> ump2;
 
         int sum = 0;
-        ump[sum]=0;
-        ump2[sum]=0;
-        for(int i =0;i<nums.size();i++)
+        ump[0]=sum;
+
+        for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
             ump[sum]=i+1;
         }
         sum = 0;
-        for(int i = nums.size()-1;i>=0;i--)
+        if(ump.find(x)!=ump.end())
+        {
+            ans = min(ans,ump[x]);
+        }
+        for(int i=nums.size()-1;i>=0;i--)
         {
             sum+=nums[i];
-            ump2[sum]=(nums.size()-i);
-            // cout<<sum<<endl;
             if(sum>x)
-            {
                 break;
-            }
-
-            int t = x - sum ;
-
+            
+            int t = x - sum;
             if(ump.find(t)!=ump.end())
             {
-                cout<<"reached "<<sum<<endl;
                 int index = ump[t];
                 if(index-1 < i)
                 {
-                    int op = (nums.size()-i+index);
-                    cout<<op<<endl;
-                    ans = min(ans,op);
+                    int op = (nums.size()-i)+index;
+                    ans= min(ans,op);
                 }
             }
         }
-        sum = 0;
-        for(int i=0;i<nums.size();i++)
-        {
-            sum+=nums[i];
-            if(sum>x)
-            {
-                break;
-            }
-            int t = x - sum ;
-            if(ump2.find(t)!=ump2.end())
-            {
-                // cout<<"reached "<<sum<<endl;
-                int index = ump2[t];
-                if(i < (nums.size()-index))
-                {
-                    int op = (i+1 +index);
-                    cout<<op<<endl;
-                    ans = min(ans,op);
-                }
-            }
-        }
+
         if(ans==INT_MAX)
-        return -1;
-        else
+            return -1;
         return ans;
             
     }
